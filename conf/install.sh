94dd3c9e4df5088aacee56afd58c278e7be9659a
smaccary <smaccary@student.42.fr>
2021-10-22 01:17:00 +0200

    up install script

--- conf/install.sh
+++ conf/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo cp ./42up.service /etc/systemd/system/. && sudo systemctl enable --now 42up
+sudo cp ./42up.service /etc/systemd/system/. && sudo systemctl daemon-reload  && sudo systemctl enable --now 42up
 systemctl status 42up
