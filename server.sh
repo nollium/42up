aa901a4af70d8becdf7f492cb8b0f9b5b3ea3ad5
smaccary <smaccary@student.42.fr>
2021-10-21 23:01:59 +0200

    better perf

--- server.sh
+++ server.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/bash
 
-if ! [ -v PORT ];then
-        PORT=8080
-fi
-
-echo Server running on port "$PORT"
-while true;
-        do
-
-CONTENT=$(./get_up_time.sh)
-OUTPUT=$(printf 'HTTP/1.1 200 OK\r\n\r\n
+make_output()
+{
+        CONTENT=$(./get_up_time.sh)
+        OUTPUT=$(printf 'HTTP/1.1 200 OK\r\n\r\n
 <html>
 <head>
 <title>intra.42.fr uptime</title>
@@ -18,10 +12,24 @@ OUTPUT=$(printf 'HTTP/1.1 200 OK\r\n\r\n
 <PRE>
 %s
 </PRE>
-<footer> backend in bash btw </footer>
+<footer><a href="https://github.com/Dirty-No/is_intra.42.fr_up"> backend in bash btw </a> </footer>
 </body>
 </html>' "$CONTENT")
-        echo CONTENT READY: "$CONTENT"
-        echo "$OUTPUT" | nc -w 1 -l "$PORT"
+        echo "($(date '+%d/%m/%Y %H:%m:%S'))" "
+------------------------------------------------------------------------------------
+$CONTENT" "
+------------------------------------------------------------------------------------" | tee -a logs
+        echo "$OUTPUT" > /tmp/.bash_http_server_content
+}
+
+if ! [ -v PORT ];then
+        PORT=8080
+fi
+
+echo Server running on port "$PORT"
+while true;
+        do
+        make_output &
+        nc -w 1 -l "$PORT" < /tmp/.bash_http_server_content
 done
 
